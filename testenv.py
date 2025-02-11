b41f57b378fc338b43a1ab603a4340b705f5f9a8
unknown <wilsonma12746@gmail.com>
2025-02-11 20:36:48 +1100

    awda

--- testenv.py
+++ testenv.py
@@ -1,1 +1,7 @@
 # attempting to get environment variables working w/ Github Secrets
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv()) # loads environment variables from the .env file 
+
+print(f'TEST_VAR => {os.getenv("TEST_VAR")}')
