bebc20879174711529551535a823e8bcf0ee7c90
unknown <wilsonma12746@gmail.com>
2025-02-23 18:54:21 +1100

    connected service account to firebase
    ENVIRONMENT VARIABLE

---
+++ firebasetest.py
@@ -0,0 +1,22 @@
+# attempting to get environment variables working w/ Github Secrets
+from dotenv import load_dotenv, find_dotenv
+import os, base64, json
+
+load_dotenv(find_dotenv()) # loads environment variables from the .env file 
+encoded_service_account_key = os.getenv("SERVICE_ACCOUNT_KEY")
+SERVICE_ACCOUNT_JSON = json.loads(base64.b64decode(encoded_service_account_key).decode('utf-8'))
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+
+# Use a service account.
+cred = credentials.Certificate(SERVICE_ACCOUNT_JSON)
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+doc_ref = db.collection("users").document("alovelace")
+doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+print('Record has been created!')
--- testenv.py
+++
@@ -1,7 +0,0 @@
-# attempting to get environment variables working w/ Github Secrets
-from dotenv import load_dotenv, find_dotenv
-import os
-
-load_dotenv(find_dotenv()) # loads environment variables from the .env file 
-
-print(f'SERIVCE_ACCOUNT_KEY => {os.getenv("SERVICE_ACCOUNT_KEY")}')
