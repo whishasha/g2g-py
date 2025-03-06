c6c7e4cf018d2e14f678e90d57af8725c6becfc8
whishasha <wilsonma12746@gmail.com>
2025-03-06 12:06:12 +1100

    testing sql database

--- firebasetest.py
+++ firebasetest.py
@@ -1,24 +1,23 @@
 # attempting to get environment variables working w/ Github Secrets
-from dotenv import load_dotenv, find_dotenv
-import os, base64, json
+# from dotenv import load_dotenv, find_dotenv
+# import os, base64, json
 
-load_dotenv(find_dotenv()) # loads environment variables from the .env file 
-encoded_service_account_key = os.getenv("SERVICE_ACCOUNT_KEY")
-SERVICE_ACCOUNT_JSON = json.loads(base64.b64decode(encoded_service_account_key).decode('utf-8'))
-
-#setting up firebase
-import firebase_admin
-from firebase_admin import firestore
-from firebase_admin import credentials
+# load_dotenv(find_dotenv()) # loads environment variables from the .env file 
+# encoded_service_account_key = os.getenv("SERVICE_ACCOUNT_KEY")
+# SERVICE_ACCOUNT_JSON = json.loads(base64.b64decode(encoded_service_account_key).decode('utf-8'))
+# deprecated code, now using SQL
 
 
-# Use a service account.
-cred = credentials.Certificate(SERVICE_ACCOUNT_JSON)
+import sqlite3
 
-app = firebase_admin.initialize_app(cred)
+remote = sqlite3.connect('database.db')
 
-db = firestore.client()
+cursor = remote.cursor()
 
-doc_ref = db.collection("users").document("apamil")
-doc_ref.set({"first": "Akhil", "last": "Pamillion", "born": 2007})
-print('Record has been created!')
+# cursor.execute('''CREATE TABLE users (name varchar(100))''')
+cursor.execute('''INSERT INTO users (name) VALUES ('hello')''')
+
+remote.commit()
+
+cursor.close()
+
