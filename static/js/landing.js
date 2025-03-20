59819c903e8f3cc8c415acc82a1b2c74b2d5ec5a
whishasha <wilsonma12746@gmail.com>
2025-03-20 14:04:01 +1100

    navbar work
    Lookin' clean!

---
+++ static/js/landing.js
@@ -0,0 +1,17 @@
+// general commands relevant for each user template
+
+// const navitem = document.getElementsByClassName('navitem');
+
+// function liftNav(e) {
+//     e.parentElement.classList.toggle('lift');
+// }
+
+// for (var i = 0; i < navitem.length; i++) {
+//     navitem[i].addEventListener("click", liftNav(navitem[i] ));
+// }
+
+const li = document.getElementsByName('li');
+for (var i = 0; i < li.length; i++) {
+li[i].innerHTML = 'hi';
+};
+
