b4a1852333e3ff2d9117a41fb1bdab567040b4c2
whishasha <wilsonma12746@gmail.com>
2025-05-04 16:22:29 +1000

    Fixed footer
    Footer now stays pushed at the bottom of each landing webpage

--- testenv.py
+++
@@ -1,6 +0,0 @@
-# attempting to get environment variables working w/ Github Secrets
-from dotenv import load_dotenv, find_dotenv
-import os, base64, json
-
-load_dotenv(find_dotenv()) # loads environment variables from the .env file 
-print(os.getenv("TEST_VAR"))
